c785adbdbf93a33b18bffc3d7d0e77eb62444bf0
Pedro Martins Trevisan <pedrotrevs@gmail.com>
2026-06-26 22:48:31 -0300

    feat: add domain error classes for account and transaction handling

---
+++ src/domain/error.ts
@@ -0,0 +1,43 @@
+abstract class DomainError extends Error {
+  abstract readonly statusCode: number;
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class AccountNotFoundError extends DomainError {
+  readonly statusCode = 404;
+
+  constructor(accountID: string) {
+    super(`Account ${accountID} not found`);
+  }
+}
+
+export class UnbalancedTransactionError extends DomainError {
+  readonly statusCode = 422;
+
+  constructor() {
+    super(
+      "Transaction entries must balance: sum of debits must equal sum of credits",
+    );
+  }
+}
+
+export class ValidationError extends DomainError {
+  readonly statusCode = 400;
+
+  constructor(message: string) {
+    super(message);
+  }
+}
+
+export class DuplicateTransactionError extends DomainError {
+  readonly statusCode = 409;
+
+  constructor(originalTransactionID: string) {
+    super(
+      `Duplicate transaction detected. Original transaction ID: ${originalTransactionID}. This operation was already processed within the last 15 minutes.`,
+    );
+  }
+}
