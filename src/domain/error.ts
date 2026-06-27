f4efd3581a0d6d9e02c4c01d50e2339864860f09
Pedro Martins Trevisan <pedrotrevs@gmail.com>
2026-06-27 00:20:42 -0300

    feat: export DomainError class and enhance ERROR stack trace handling

--- src/domain/error.ts
+++ src/domain/error.ts
@@ -1,8 +1,13 @@
-abstract class DomainError extends Error {
+export abstract class DomainError extends Error {
   abstract readonly statusCode: number;
 
   constructor(message: string) {
     super(message);
+    this.name = this.constructor.name;
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
+    }
   }
 }
 
