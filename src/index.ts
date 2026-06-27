4bbbfe780e34b8898f664c9141b4ae0102d2d195
Pedro Martins Trevisan <pedrotrevs@gmail.com>
2026-06-27 01:43:34 -0300

    feat: enhance server setup with secure headers and error handling

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,24 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { secureHeaders } from "hono/secure-headers";
+import { errorHandler } from "./shared/middlewares/error-handler.js";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use("*", secureHeaders());
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.onError(errorHandler);
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  },
+);
