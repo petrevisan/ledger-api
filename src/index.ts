ea71bc639172fd9c7c540dc092d0c30a63b4e3ea
Pedro Martins Trevisan <pedrotrevs@gmail.com>
2026-06-29 22:54:10 -0300

    feat: rename TransactionsRoutes to EventsRoutes in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 import { secureHeaders } from "hono/secure-headers";
 import { errorHandler } from "./shared/middlewares/error-handler.js";
 import { IdentityRoutes } from "./modules/identity/identity.http.js";
-import { TransactionsRoutes } from "./modules/ledger/events/events.http.js";
+import { EventsRoutes } from "./modules/ledger/events/events.http.js";
 
 const app = new Hono();
 
@@ -14,7 +14,7 @@ app.get("/", (c) => {
 });
 
 app.route("/identity", IdentityRoutes());
-app.route("/transactions", TransactionsRoutes());
+app.route("/events", EventsRoutes());
 
 app.onError(errorHandler);
 
